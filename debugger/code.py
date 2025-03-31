35d3d3e1824320b5354cd18249bbf4933d5b0c75
taylorj8 <taylorj8@tcd.ie>
2025-03-31 15:09:19 +0100

    added a tutorial for the debugger

---
+++ debugger/code.py
@@ -0,0 +1,66 @@
+import pytch
+import random
+
+score = 0
+lives = 3
+
+game_over = False
+
+class Alien(pytch.Sprite):
+    Costumes = ["friendly-alien.png", "enemy-alien.png"]
+    Sounds = ["explosion.mp3", "scream.mp3"]
+
+    @pytch.when_I_receive("make-clones")
+    def make_clones(self):
+        for i in range(5):
+            self.go_to_xy(-150 + i * 60, 180)
+            pytch.create_clone_of(self)
+        self.go_to_xy(150, 180)
+
+    @pytch.when_I_receive("play-game")
+    def drift_down_screen(self):
+        self.set_y(180)
+        while not game_over:
+            self.show()
+            glide_time = random.uniform(3.0, 5.0)
+            self.glide_to_xy(self.x_position, -180, glide_time)
+            self.switch_costume(random.choice([0, 1]))
+
+    @pytch.when_this_sprite_clicked
+    def handle_hit(self):
+        if self.costume_number == 2:
+            self.start_sound("explosion")
+            global score
+            score += 10
+        else:
+            self.lose_life()
+        self.hide()
+
+    def lose_life(self):
+        self.start_sound("scream")
+        global lives
+        lives -= 1
+        if lives == 0:
+            global game_over
+            game_over = True
+            pytch.broadcast("game-over")
+
+    @pytch.when_I_receive("game-over")
+    def do_game_over(self):
+        self.hide()
+
+
+class Galaxy(pytch.Stage):
+    Backdrops = ["starry-sky.jpg"]
+    Sounds = ["fizz.mp3"]
+
+    @pytch.when_green_flag_clicked
+    def run(self):
+        pytch.show_variable(None, "score")
+        pytch.show_variable(None, "lives", right=236)
+        pytch.broadcast_and_wait("make-clones")
+        pytch.broadcast("play-game")
+
+    @pytch.when_stage_clicked
+    def make_miss_sound(self):
+        self.start_sound("fizz")
